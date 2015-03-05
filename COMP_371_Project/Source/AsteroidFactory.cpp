a69461f5e97df1bdbef4a0e8b60d97cabcff7890
Archimedyz <awais1994@hotmail.com>
2015-03-05 14:14:22 -0500

    Randomized all aspects of asteroids in AsteroidFactory::ceateAsteroid()

--- COMP_371_Project/Source/AsteroidFactory.cpp
+++ COMP_371_Project/Source/AsteroidFactory.cpp
@@ -4,23 +4,40 @@
 
 using namespace std;
 
+const float AsteroidFactory::ALPHA_MAX = 90.0f;
+const float AsteroidFactory::ALPHA_MIN = -90.0f;
+const float AsteroidFactory::BETA_MAX = 360.0f;
+const float AsteroidFactory::BETA_MIN = 0.0f;
+const float AsteroidFactory::SCALE_MAX = 2.5f;
+const float AsteroidFactory::SCALE_MIN = 0.5f;
+const float AsteroidFactory::ROTATION_SPEED_MAX = 120.0f;
+const float AsteroidFactory::ROTATION_SPEED_MIN = 40.0f;
+const float AsteroidFactory::SPEED_MAX = 7.5f;
+const float AsteroidFactory::SPEED_MIN = 2.5f;
+
 AsteroidFactory::AsteroidFactory(){}
 AsteroidFactory::~AsteroidFactory(){}
 AsteroidModel* AsteroidFactory::createAsteroid(){
 	AsteroidModel* asteroid = new AsteroidModel();
 
 	//Randomize these Elements
-	int alpha = rand();
-	int beta = rand();
-	float x = cosf((float)alpha)*cosf((float)beta);
+	float alpha = ALPHA_MIN + static_cast<float>(rand()) / static_cast<float>(RAND_MAX/(ALPHA_MAX - ALPHA_MIN));
+	float beta = BETA_MIN + static_cast<float>(rand()) / static_cast<float>(RAND_MAX / (BETA_MAX - BETA_MIN));
+	float x = cosf(alpha)*cosf(beta);
 	float y = sinf(alpha);
 	float z = -cosf(alpha)*sinf(beta);
+	float scale_x = SCALE_MIN + static_cast<float>(rand()) / static_cast<float>(RAND_MAX / (SCALE_MAX - SCALE_MIN));
+	float scale_y = SCALE_MIN + static_cast<float>(rand()) / static_cast<float>(RAND_MAX / (SCALE_MAX - SCALE_MIN));
+	float scale_z = SCALE_MIN + static_cast<float>(rand()) / static_cast<float>(RAND_MAX / (SCALE_MAX - SCALE_MIN));
 	glm::vec3 initialPosition = Variables::InnerRadius*glm::normalize(glm::vec3(x, y, z)) - Variables::WorldCenter;
-	glm::vec3 scale = glm::vec3(1.0f, 1.0f, 1.0f);
+	glm::vec3 scale = glm::vec3(scale_x, scale_y, scale_z);
 	glm::vec3 rotationAxis = glm::vec3(x*rand(), -y*rand(), z*rand());
-	float initialRotationAngle = 20.0f;
-	float rotationSpeed = 40.0f;
-	float speed = 1.2f * (rand() % 5);
+	//randomize initial rotation angle.
+	float initialRotationAngle = static_cast<float>(rand()) / static_cast<float>(RAND_MAX / 360.0f);
+	// randomized rotation speed
+	float rotationSpeed = ROTATION_SPEED_MIN + static_cast<float>(rand()) / static_cast<float>(RAND_MAX / (ROTATION_SPEED_MAX - ROTATION_SPEED_MIN));
+	// randomized speed
+	float speed = SPEED_MIN + static_cast<float>(rand()) / static_cast<float>(RAND_MAX / (SPEED_MAX - SPEED_MIN));
 
 	asteroid->SetPosition(initialPosition);
 	asteroid->SetRotation(rotationAxis, initialRotationAngle);
--- COMP_371_Project/Source/AsteroidFactory.h
+++ COMP_371_Project/Source/AsteroidFactory.h
@@ -8,4 +8,15 @@ public:
 	static AsteroidModel* createAsteroid();
 private:
 	AsteroidFactory();
+
+	static const float ALPHA_MAX;
+	static const float ALPHA_MIN;
+	static const float BETA_MAX;
+	static const float BETA_MIN;
+	static const float SCALE_MAX;
+	static const float SCALE_MIN;
+	static const float ROTATION_SPEED_MAX;
+	static const float ROTATION_SPEED_MIN;
+	static const float SPEED_MAX;
+	static const float SPEED_MIN;
 };
